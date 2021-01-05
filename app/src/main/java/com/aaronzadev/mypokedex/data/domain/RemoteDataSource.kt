0c39429fdd2408f6a75d2f3f101bc3cb3515bfa6
aaron-za-dev <aaronzadev@gmail.com>
2021-01-05 16:21:03 -0600

    Update RemoteDataSource interface
    Add a new param to getPokemons() function offset = pagenumber and limit = pagesize or items per page

--- app/src/main/java/com/aaronzadev/mypokedex/data/domain/RemoteDataSource.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/data/domain/RemoteDataSource.kt
@@ -1,5 +1,8 @@
 package com.aaronzadev.mypokedex.data.domain
 
+//offset = pagenumber or position in pagination api
+//limit = pagesize or number of items per page
+
 interface RemoteDataSource {
-    suspend fun getPokemons(page: Int) : List<Pokemon>
+    suspend fun getPokemons(offset:  Int, limit: Int) : List<Pokemon>
 }
