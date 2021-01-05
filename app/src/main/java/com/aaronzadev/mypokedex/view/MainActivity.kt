a31edaf8531f9ad7c231a01547f849353a791bf4
aaron-za-dev <aaronzadev@gmail.com>
2021-01-05 16:34:44 -0600

    Update MainActivity class
    Change the way to access to MainViewModel class and add necessary imports

--- app/src/main/java/com/aaronzadev/mypokedex/view/MainActivity.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/view/MainActivity.kt
@@ -1,25 +1,43 @@
 package com.aaronzadev.mypokedex.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aaronzadev.mypokedex.PokedexApp
 import com.aaronzadev.mypokedex.R
+import com.aaronzadev.mypokedex.data.domain.Pokemon
+import com.aaronzadev.mypokedex.data.domain.PokemonsRepository
 import com.aaronzadev.mypokedex.view.viewmodel.MainViewModel
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel : ViewModel
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel : MainViewModel by viewModels()
+        //val viewModel : MainViewModel by viewModels()
+        //val pokemonsRepository = PokemonsRepository()
+        //viewModel = ViewModelProvider(this)
+
+        lifecycleScope.launch {
+            viewModel.pokemons.collect { pokemons ->
+                for (currentPokemon : Pokemon in pokemons){
+                    Log.d("POKE", currentPokemon.name)
+                }
+            }
+        }
 
         var mlayoutManager = LinearLayoutManager(this)
         val recycler = findViewById<RecyclerView>(R.id.pokerecycler).apply {
