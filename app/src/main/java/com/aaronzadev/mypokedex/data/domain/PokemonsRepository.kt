4d4dea2e8883e637f3c31b1550dd45771f97d160
aaron-za-dev <aaronzadev@gmail.com>
2021-01-05 16:19:18 -0600

    Update PokemonsRepository class
    Remove th keyword suspend from function getPokemons() and remove unnecesary properties

--- app/src/main/java/com/aaronzadev/mypokedex/data/domain/PokemonsRepository.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/data/domain/PokemonsRepository.kt
@@ -7,16 +7,18 @@ import kotlinx.coroutines.withTimeout
 class PokemonsRepository(private val localDataSource: LocalDataSource,
                          private val remoteDataSource: RemoteDataSource) {
 
-    private val LIMIT = 20
-    private val OFFSET = 10
+    //private val DEFAULTOFFSET = 0 //pageNumber
+    private val DEFAULTLIMIT = 20 //pageSize or items per page
 
-    suspend fun getPokemons(): Flow<List<Pokemon>> = localDataSource.getPokemons()
+    fun getPokemons(): Flow<List<Pokemon>> = localDataSource.getPokemons()
 
-    suspend fun checkRequireNewPage(lastVisible: Int) {
+    suspend fun checkRequireNewPage(lastVisiblePokemon: Int) {
         val size = localDataSource.size()
-        if (lastVisible >= size - OFFSET) {
-            val page = size / LIMIT + 1
-            val newPokemons = withTimeout(5_000) { remoteDataSource.getPokemons(page) }
+        if (lastVisiblePokemon >= size - DEFAULTLIMIT) {
+            val page = size / DEFAULTLIMIT + 1
+            val newPokemons = withTimeout(5_000) {
+                remoteDataSource.getPokemons(page, DEFAULTLIMIT)
+            }
             localDataSource.saveMovies(newPokemons)
         }
     }
