d5c93e2e3f20154ba66b80868230bd9ce8355645
aaron-za-dev <aaronzadev@gmail.com>
2021-01-05 16:25:04 -0600

    Update PokemonService interface
    Set default values for the getPokemonList() function parameters an reformart code

--- app/src/main/java/com/aaronzadev/mypokedex/data/remote/PokemonService.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/data/remote/PokemonService.kt
@@ -5,9 +5,14 @@ import retrofit2.http.Query
 
 interface PokemonService {
 
+    //offset = pagenumber or position in pagination api default = 0
+    //limit = pagesize or number of items per page default = 20
+
     @GET(value = "pokemon?")
     //@GET(value = "pokemon?offset=0&limit=20")
-    suspend fun getPokemonList(@Query("offset") offset : String,
-                               @Query("limit") limit : String) : PokemonListResponse
+    suspend fun getPokemonList(
+        @Query("offset") offset: Int = 0,
+        @Query("limit") limit: Int = 20
+    ): PokemonListResponse
 
 }
