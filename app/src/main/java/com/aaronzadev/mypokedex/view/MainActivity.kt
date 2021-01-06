6e95bc1bcd8229812e977d803f833d293d5a246a
Aarón Zúñiga <aaronzadev@gmail.com>
2021-01-05 21:59:26 -0600

    Update MainActivity Class
    Create a new function for init the viewModel object and remove unnecesary code

--- app/src/main/java/com/aaronzadev/mypokedex/view/MainActivity.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/view/MainActivity.kt
@@ -2,18 +2,17 @@ package com.aaronzadev.mypokedex.view
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.aaronzadev.mypokedex.PokedexApp
 import com.aaronzadev.mypokedex.R
 import com.aaronzadev.mypokedex.data.domain.Pokemon
 import com.aaronzadev.mypokedex.data.domain.PokemonsRepository
+import com.aaronzadev.mypokedex.data.local.LocalDataSourceImpl
+import com.aaronzadev.mypokedex.data.remote.RemoteDataSourceImpl
+import com.aaronzadev.mypokedex.util.app
+import com.aaronzadev.mypokedex.view.adapters.PokemonAdapter
 import com.aaronzadev.mypokedex.view.viewmodel.MainViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
@@ -22,6 +21,7 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private val pokemons = mutableListOf<Pokemon>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +29,28 @@ class MainActivity : AppCompatActivity() {
 
         //val viewModel : MainViewModel by viewModels()
         //val pokemonsRepository = PokemonsRepository()
-        //viewModel = ViewModelProvider(this)
+        viewModel = createViewModel()
+
+        val pokemonAdapter = PokemonAdapter(pokemons)
 
         lifecycleScope.launch {
-            viewModel.pokemons.collect { pokemons ->
-                for (currentPokemon : Pokemon in pokemons){
-                    Log.d("POKE", currentPokemon.name)
-                }
+            viewModel.pokemons.collect {
+                pokemons.addAll(it)
+                pokemonAdapter.notifyDataSetChanged()
             }
         }
 
-        var mlayoutManager = LinearLayoutManager(this)
+        val mlayoutManager = LinearLayoutManager(this)
+
         val recycler = findViewById<RecyclerView>(R.id.pokerecycler).apply {
             layoutManager = mlayoutManager
             setHasFixedSize(true)
-
+            adapter = pokemonAdapter
         }
 
 
     }
+
+    private fun createViewModel() = MainViewModel(PokemonsRepository(LocalDataSourceImpl(app.pokemonDB), RemoteDataSourceImpl()))
 
 }
