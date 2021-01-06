a1fcdf67d115bd31e85e0fe5b262715f386e25e3
Aarón Zúñiga <aaronzadev@gmail.com>
2021-01-05 21:55:53 -0600

    Update PokedexApp class
    Rename the property

--- app/src/main/java/com/aaronzadev/mypokedex/PokedexApp.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/PokedexApp.kt
@@ -6,12 +6,12 @@ import com.aaronzadev.mypokedex.data.local.PokemonDB
 
 class PokedexApp : Application() {
 
-    lateinit var pokemonDatabase : PokemonDB
+    lateinit var pokemonDB : PokemonDB
         private set
 
     override fun onCreate() {
         super.onCreate()
-        pokemonDatabase = Room.databaseBuilder(this, PokemonDB::class.java, "pokemondb").build()
+        pokemonDB = Room.databaseBuilder(this, PokemonDB::class.java, "pokemondb").build()
     }
 
 }
