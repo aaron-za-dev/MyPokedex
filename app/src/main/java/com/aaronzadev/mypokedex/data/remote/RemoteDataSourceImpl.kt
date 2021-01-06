80715d059e1742f63001266420fbec0bf62cc8fa
Aarón Zúñiga <aaronzadev@gmail.com>
2021-01-05 21:55:13 -0600

    Update RemoteDataSourceImpl
    Remove redundant empty constructor and rename function params

--- app/src/main/java/com/aaronzadev/mypokedex/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/data/remote/RemoteDataSourceImpl.kt
@@ -4,8 +4,8 @@ import com.aaronzadev.mypokedex.data.domain.Pokemon
 import com.aaronzadev.mypokedex.data.domain.RemoteDataSource
 import com.aaronzadev.mypokedex.util.toDomainPokemon
 
-class RemoteDataSourceImpl() : RemoteDataSource {
+class RemoteDataSourceImpl : RemoteDataSource {
 
-    override suspend fun getPokemons(offset: Int, size: Int): List<Pokemon> =
-        PokemonServiceImpl.service.getPokemonList(offset, size).results.map{ it.toDomainPokemon() }
+    override suspend fun getPokemons(offset: Int, limit: Int): List<Pokemon> =
+        PokemonServiceImpl.service.getPokemonList(offset, limit).results.map{ it.toDomainPokemon() }
 }
