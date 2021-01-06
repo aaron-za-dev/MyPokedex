c12c80d069a9df2cab9a0eb5385a22e86a8aa459
Aarón Zúñiga <aaronzadev@gmail.com>
2021-01-05 21:58:12 -0600

    Update PokemonAdapter
    Implement the functions of adapter and completes PokemonHolder class

--- app/src/main/java/com/aaronzadev/mypokedex/view/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/view/adapters/PokemonAdapter.kt
@@ -1,24 +1,35 @@
 package com.aaronzadev.mypokedex.view.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.aaronzadev.mypokedex.R
+import com.aaronzadev.mypokedex.data.domain.Pokemon
 
-class PokemonAdapter() : RecyclerView.Adapter<PokemonHolder>() {
+class PokemonAdapter(private val pokemons : MutableList<Pokemon>) : RecyclerView.Adapter<PokemonHolder>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent,false)
+        return PokemonHolder(view)
     }
 
     override fun onBindViewHolder(holder: PokemonHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.txtPokemon.text = pokemons[position].name
+        holder.imgPokemon.setImageResource(R.drawable.pokeball)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return pokemons.size
     }
 
 }
 
 class PokemonHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val txtPokemon = itemView.findViewById<TextView>(R.id.tv_pokemon_name)
+    val imgPokemon = itemView.findViewById<ImageView>(R.id.im_pokemon)
 
 }
