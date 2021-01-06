4bb1ded110e253373572efcb90544f3257363f86
Aarón Zúñiga <aaronzadev@gmail.com>
2021-01-05 21:57:00 -0600

    Update DataConverter file
    Reformat all code

--- app/src/main/java/com/aaronzadev/mypokedex/util/DataConverter.kt
+++ app/src/main/java/com/aaronzadev/mypokedex/util/DataConverter.kt
@@ -1,11 +1,16 @@
 package com.aaronzadev.mypokedex.util
 
+import android.content.Context
+import com.aaronzadev.mypokedex.PokedexApp
 import com.aaronzadev.mypokedex.data.domain.Pokemon
+import com.aaronzadev.mypokedex.data.local.RoomPokemon
 import com.aaronzadev.mypokedex.data.remote.Result
 
-fun Result.toDomainPokemon(): Pokemon =
-        Pokemon(
-            0,
-            name,
-            url
-        )
+fun Result.toDomainPokemon(): Pokemon = Pokemon(0, name, url)
+
+fun RoomPokemon.toDomainPokemon(): Pokemon = Pokemon(id, name, url)
+
+fun Pokemon.toRoomPokemon(): RoomPokemon = RoomPokemon(id, name, url)
+
+val Context.app: PokedexApp
+    get() = applicationContext as PokedexApp
